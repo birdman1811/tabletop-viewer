34b03613846b8421b614330a09c92d11815ff867
Chris <trolltrader@gmail.com>
2019-01-23 11:48:47 +0000

    commit

---
+++ src/Player.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Player extends React.Component {
+    constructor(props) {
+        this.state = {
+        name = props.name,
+        phone = props.phone,
+        props = props.session
+    };
+}
+
+    editName(newName){
+        this.state.name = newName;
+    }
+
+    editPhone(newPhone){
+        this.state.phone = newPhone;
+    }
+
+    render(){
+        return (
+            <div className="player">
+            <span>{name}</span>
+            <span>{phone}</span>
+            <span>{session}</span>
+            </div>
+        )
+    }
+}
---
+++ src/PlayerList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class PlayerList extends React.Component {
+
+}
